88b9fb3b82c113e7d063024b5688c87e3b4f00ae
rudrashisdutta <rudra2001official@gmail.com>
2021-08-28 11:01:41 -0400

    DAA Lab6 Completed.
    28-08-2021 08-31p

--- DAA-LAB/Lab6/1.c
+++ DAA-LAB/Lab6/1.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
 clock_t start, end;
 double cpu_time_used;
 int randomiser = 0;
-int *arr;
+int *arr_354;
 void generate(int n)
 {
     randomiser++;
     srand(n + randomiser);
-    free(arr);
-    arr = (int *)malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++)
-        *(arr + i) = rand();
+    free(arr_354);
+    arr_354 = (int *)malloc(n * sizeof(int));
+    int i;
+    for (i = 0; i < n; i++)
+        *(arr_354 + i) = rand();
 }
 void swap(int *x, int *y)
 {
@@ -20,130 +21,111 @@ void swap(int *x, int *y)
     (*x) = (*y);
     (*y) = t;
 }
-void heapifylarge(int arr[], int n, int i)
+void heapifylarge(int arr_354[], int n, int i)
 {
     int largest = i;
     int l = 2 * i + 1;
     int r = 2 * i + 2;
-
-    if (l < n && arr[l] > arr[largest])
+    if (l < n && arr_354[l] > arr_354[largest])
         largest = l;
-
-    if (r < n && arr[r] > arr[largest])
+    if (r < n && arr_354[r] > arr_354[largest])
         largest = r;
-
     if (largest != i)
     {
-        swap(&arr[i], &arr[largest]);
-        heapifylarge(arr, n, largest);
+        swap(&arr_354[i], &arr_354[largest]);
+        heapifylarge(arr_354, n, largest);
     }
 }
-void minheapSort(int arr[], int n)
+void minheapSort(int arr_354[], int n)
 {
     for (int i = n / 2 - 1; i >= 0; i--)
-        heapifylarge(arr, n, i);
-
+        heapifylarge(arr_354, n, i);
     for (int i = n - 1; i >= 0; i--)
     {
-        swap(&arr[0], &arr[i]);
-        heapifylarge(arr, i, 0);
+        swap(&arr_354[0], &arr_354[i]);
+        heapifylarge(arr_354, i, 0);
     }
 }
-void heapifysmall(int arr[], int n, int i)
+void heapifysmall(int arr_354[], int n, int i)
 {
-    int largest = i;
+    int smallest = i;
     int l = 2 * i + 1;
     int r = 2 * i + 2;
-    if (l < n && arr[l] < arr[largest])
-        largest = l;
-
-    if (r < n && arr[r] < arr[largest])
-        largest = r;
-
-    if (largest != i)
+    if (l < n && arr_354[l] < arr_354[smallest])
+        smallest = l;
+    if (r < n && arr_354[r] < arr_354[smallest])
+        smallest = r;
+    if (smallest != i)
     {
-        swap(&arr[i], &arr[largest]);
-        heapifysmall(arr, n, largest);
+        swap(&arr_354[i], &arr_354[smallest]);
+        heapifysmall(arr_354, n, smallest);
     }
 }
-void maxheapSort(int arr[], int n)
+void maxheapSort(int arr_354[], int n)
 {
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapifysmall(arr, n, i);
-
-    for (int i = n - 1; i >= 0; i--)
+    int i;
+    for (i = n / 2 - 1; i >= 0; i--)
+        heapifysmall(arr_354, n, i);
+    for (i = n - 1; i >= 0; i--)
     {
-        swap(&arr[0], &arr[i]);
-        heapifysmall(arr, i, 0);
+        swap(&arr_354[0], &arr_354[i]);
+        heapifysmall(arr_354, i, 0);
     }
 }
-void printArray(int arr[], int n)
+void printarr(int arr_354[], int n)
 {
-    for (int i = 0; i < n; ++i)
-        printf("\n%d ", arr[i]);
+    int i;
+    for (i = 0; i < n; ++i)
+        printf("\n%d ", arr_354[i]);
     printf("\n");
 }
-void largest(int arr[], int n)
+void largest(int arr_354[], int n)
 {
-    int large = arr[0];
-    for (int i = 0; i < n; i++)
-        if (large < arr[i])
-            large = arr[i];
+    int large = arr_354[0];
+    int i;
+    for (i = 0; i < n; i++)
+        if (large < arr_354[i])
+            large = arr_354[i];
     printf("\nLargest element is %d\n", large);
 }
-void sorting(int arr[], int n)
+void sorting(int arr_354[], int n)
 {
-    int choice;
-    printf("\n1.Max heap\n2.Min heap\nEnter your choice:");
-    scanf("%d", &choice);
-sorting:
-    switch (choice)
-    {
-    case 1:
-        maxheapSort(arr, n);
-        break;
-    case 2:
-        minheapSort(arr, n);
-        break;
-    default:
-        printf("\nEnter correct choiceion.");
-        goto sorting;
-        break;
-    }
+    maxheapSort(arr_354, n);
 }
-void replace(int arr[], int n)
+void replace(int arr_354[], int n)
 {
     int node, value;
     printf("\nEnter node:");
     scanf("%d", &node);
     printf("\nEnter value:");
     scanf("%d", &value);
-    arr[node] = value;
-    sorting(arr, n);
+    arr_354[node] = value;
+    sorting(arr_354, n);
 }
-void insert(int arr[], int *n)
+void insert(int arr_354[], int *n)
 {
     int new;
     int size = (*n) + 1;
     int *store = (int *)malloc(size * sizeof(int));
     for (int i = 0; i < (*n); i++)
-        *(store + i) = arr[i];
+        *(store + i) = arr_354[i];
     printf("\nEnter new element:");
     scanf("%d", &new);
     *(store + size - 1) = new;
     generate(size);
     for (int i = 0; i < size; i++)
-        arr[i] = *(store + i);
+        arr_354[i] = *(store + i);
     *(n) = size;
-    sorting(arr, *n);
+    sorting(arr_354, *n);
 }
-int delete (int arr[], int *n)
+int delete (int arr_354[], int *n)
 {
     int node = -1, value;
     printf("\nEnter value to delete:");
     scanf("%d", &value);
-    for (int i = 0; i < (*n); i++)
-        if (arr[i] == value)
+    int i;
+    for (i = 0; i < (*n); i++)
+        if (arr_354[i] == value)
         {
             node = i;
             break;
@@ -154,100 +136,115 @@ int delete (int arr[], int *n)
         return 1;
     }
     int *store = (int *)malloc(((*n) - 1) * sizeof(int));
-    for (int i = 0, j = 0; i < (*n); i++)
+    int j;
+    for (i = 0, j = 0; i < (*n); i++)
         if (i != node)
         {
-            *(store + j) = arr[i];
+            *(store + j) = arr_354[i];
             j++;
         }
     generate((*n) - 1);
     *n = (*n) - 1;
-    for (int i = 0; i < (*n); i++)
-        arr[i] = *(store + i);
-    sorting(arr, *n);
+    for (i = 0; i < (*n); i++)
+        arr_354[i] = *(store + i);
+    sorting(arr_354, *n);
     return 0;
 }
 void main()
 {
-    int n, choice;
-    arr = (int *)malloc(10000000 * sizeof(int));
+    int n, choice = -1;
+    arr_354 = (int *)malloc(10000000 * sizeof(int));
     printf("\nEnter number of elements:");
     scanf("%d", &n);
     generate(n);
-    while (choice != 13)
+    while (choice != 0)
     {
-        printf("\n0.Exit\n1.Generate\n2.Display\n3.Max-heap(Descending)\n4.Min-heap(ascending)\n5.time to sort random data in descending\n6.\n7.\n8.Total time complexity\n9.Extract largest\n10.Replace value\n11.Insert a new element\n12.Delete an element\nEnter your choice:");
+        printf("\n\n\nRoll Number: 1906354\n0> Exit\n1> Generate\n2> Display\n3> Descending\n4> Ascending\n5> time to sort random data in ascending\n6> Time to sort ascending from ascending order\n7> Time to sort ascending from descending order\n8> Total time complexity\n9> Extract largest\n10> Replace value\n11> Insert a new element\n12> Delete an element\n\nEnter your choice:");
         scanf("%d", &choice);
+        printf("\n\n");
         switch (choice)
         {
+        case 0:
+            return;
         case 1:
             printf("\nEnter number of elements:");
             scanf("%d", &n);
             generate(n);
             break;
         case 2:
-            printArray(arr, n);
+            printarr(arr_354, n);
             break;
         case 3:
-            maxheapSort(arr, n);
+            maxheapSort(arr_354, n);
             break;
         case 4:
-            minheapSort(arr, n);
+            minheapSort(arr_354, n);
             break;
         case 5:
             generate(n);
             start = clock();
-            maxheapSort(arr, n);
+            minheapSort(arr_354, n);
             end = clock();
             cpu_time_used = ((double)(end - start) / CLOCKS_PER_SEC);
-            printf("\n%lf\n", cpu_time_used);
+            printf("%lf", cpu_time_used);
             break;
         case 6:
+            minheapSort(arr_354, n);
+            start = clock();
+            minheapSort(arr_354, n);
+            end = clock();
+            cpu_time_used = ((double)(end - start) / CLOCKS_PER_SEC);
+            printf("%lf", cpu_time_used);
             break;
         case 7:
+            maxheapSort(arr_354, n);
+            start = clock();
+            minheapSort(arr_354, n);
+            end = clock();
+            cpu_time_used = ((double)(end - start) / CLOCKS_PER_SEC);
+            printf("%lf", cpu_time_used);
             break;
         case 8:
             printf("\nElements\tData random\tData ascending\tData descending\n");
-            for (int i = 5000; i <= 50000; i = i + 5000)
+            int i;
+            for (i = 5000; i <= 50000; i = i + 5000)
             {
                 n = i;
                 generate(n);
                 printf("%d\t\t", i);
                 start = clock();
-                minheapSort(arr, n);
+                minheapSort(arr_354, n);
                 end = clock();
                 cpu_time_used = ((double)(end - start) / CLOCKS_PER_SEC);
                 printf("%lf\t", cpu_time_used);
                 start = clock();
-                minheapSort(arr, n);
+                minheapSort(arr_354, n);
                 end = clock();
                 cpu_time_used = ((double)(end - start) / CLOCKS_PER_SEC);
                 printf("%lf\t", cpu_time_used);
-                maxheapSort(arr, n);
+                maxheapSort(arr_354, n);
                 start = clock();
-                minheapSort(arr, n);
+                minheapSort(arr_354, n);
                 end = clock();
                 cpu_time_used = ((double)(end - start) / CLOCKS_PER_SEC);
                 printf("%lf\n", cpu_time_used);
             }
             break;
         case 9:
-            largest(arr, n);
+            largest(arr_354, n);
             break;
         case 10:
-            replace(arr, n);
+            replace(arr_354, n);
             break;
         case 11:
-            insert(arr, &n);
+            insert(arr_354, &n);
             break;
         case 12:
-            printArray(arr, n);
-            delete (arr, &n);
+            delete (arr_354, &n);
             break;
-        case 0:
-            return;
+
         default:
-            printf("\nWrong choiceion entered.");
+            printf("\nWrong option entered.");
         }
     }
 }
---
+++ DAA-LAB/Lab6/README.md
@@ -0,0 +1,32 @@
+# `DAA LAB5`
+
+---
+
+## `Questions`
+
+###### Question1: [`Answer1`](1.c)
+
+    ✨ Write a menu (given as follows) driven program to sort an array of n integers in ascending order by heap sort algorithm and perform the operations on max heap.
+        Determine the time required to sort the elements. Repeat the experiment for different values of n, the number of elements in the array to be sorted and plot a graph of the time taken versus n.
+        The elements can be read from a file or can be generated using the random number generator.
+
+        MAX-HEAP & PRIORITY QUEUE MENU:
+            0. Quit
+            1. n Random numbers=>Array
+            2. Display the Array
+            3. Sort the Array in Ascending Order by using Max-Heap Sort technique
+            4. Sort the Array in Descending Order by using any algorithm
+            5. Time Complexity to sort ascending of random data
+            6. Time Complexity to sort ascending of data already sorted in ascending order
+            7. Time Complexity to sort ascending of data already sorted in descending order
+            8. Time Complexity to sort ascending all Cases (Data Ascending, Data in Descending & Random Data) in Tabular form for values n=5000 to 50000, step=5000
+            9. Extract largest element
+            10. Replace value at a node with new value
+            11. Insert a new element
+            12. Delete an element
+
+##
+
+###### `NOTE: YOU CAN CLICK ON ANY [Answer*], to access the answer to the corresponding question`
+
+---
