0e8cb6d6b2e2cb4db8155f8b2b8dbf713c3317bf
rudrashisdutta <rudra2001official@gmail.com>
2021-09-01 03:35:53 -0400

    CN Lab4 minor updates
    01-09-2021 01-05p

--- CN-LAB/Lab4/3/client.c
+++ CN-LAB/Lab4/3/client.c
@@ -14,7 +14,7 @@ int main()
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   sa_addr.sin_family = AF_INET;
   sa_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  sa_addr.sin_port = htons(6000);
+  sa_addr.sin_port = htons(6002);
   memset(sa_addr.sin_zero, '\0', sizeof sa_addr.sin_zero);
   i = connect(sockfd, (struct sockaddr *)&sa_addr, sizeof(sa_addr));
   for (i = 0; i < 100; i++)
